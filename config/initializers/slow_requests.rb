4a1e5343698997e067854bcb03b1b393092be44c
Bryce McLean <bryce.mclean@bitstrata.com>
2020-12-18 15:31:22 -0600

    Adding slow_requests.

--- config/initializers/slow_requests.rb
+++ config/initializers/slow_requests.rb
@@ -1,4 +1,4 @@
-Rack::Timeout.register_state_change_observer(:slowpoke) do |env|
+Rack::Timeout.register_state_change_observer(:slow_requests) do |env|
   if env[Rack::Timeout::ENV_INFO_KEY].state == :timed_out
     puts "****************** timed out!!!!! ******************"
   elsif env[Rack::Timeout::ENV_INFO_KEY].state == :expired
