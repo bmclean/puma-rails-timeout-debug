07377f5bda99cc6ca0b12a0a5f35469908fdb211
Bryce McLean <bryce.mclean@bitstrata.com>
2021-01-06 11:51:20 -0600

    Adding logging of Puma request_body_wait time.

--- config/initializers/slow_requests.rb
+++ config/initializers/slow_requests.rb
@@ -1,7 +1,8 @@
 Rack::Timeout.register_state_change_observer(:slow_requests) do |env|
+  puts "\e[36mPuma request body wait time (ms): #{env["puma.request_body_wait"]}\e[0m"
   if env[Rack::Timeout::ENV_INFO_KEY].state == :timed_out
-    puts "****************** timed out!!!!! ******************"
+    puts "****************** Rack::Timeout state is timed out!!!!! ******************"
   elsif env[Rack::Timeout::ENV_INFO_KEY].state == :expired
-    puts "****************** expired!!!!! ******************"
+    puts "****************** Rack::Timeout state is expired!!!!! ******************"
   end
 end
